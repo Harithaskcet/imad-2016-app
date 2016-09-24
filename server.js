181b5d28202746e6cde7ad3521c624c26ca5ccf8
Harithaskcet <15eucs034@skcet.ac.in>
2016-09-24 13:53:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/articleone',function (req, res){
 });
 
 app.get('/articletwo',function (req, res){
-    res.send('Article two is ready and it is displayed');
+    res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 
 app.get('/articlethree',function (req, res){
-    res.send('Article three is ready and it is displayed');
+    res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
